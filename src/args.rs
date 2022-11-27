41053ee8d1a3d69fb6b96583a40923d162759a27
banditpig <mike_k_houghton@yahoo.co.uk>
2022-11-27 09:53:36 +0000

    toml app config tests.

--- src/args.rs
+++ src/args.rs
@@ -3,14 +3,14 @@ use clap::*;
 use serde_derive::Deserialize;
 use std::fmt::{Display, Formatter};
 use std::fs;
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
 }
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 pub enum Commands {
     /// does testing things
     File {
@@ -54,14 +54,52 @@ impl Display for Arguments {
     }
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Configuration {
     pub args: Arguments,
 }
 impl Configuration {
-    pub fn new(fname: &str) -> Result<Self, VideoError> {
-        let contents = fs::read_to_string(fname)?;
+    pub fn new(name: &str) -> Result<Self, VideoError> {
+        let contents = fs::read_to_string(name)?;
         let config: Configuration = toml::from_str(&contents)?;
         Ok(config)
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use claim::*;
+    #[test]
+    fn config_ok() {
+        let config = Configuration::new("testfiles/dummyconfig/good.toml");
+        assert_ok!(&config);
+
+        let cfg = config.unwrap().args;
+        assert_eq!(cfg.client_folder, "client");
+        assert_eq!(cfg.dummies_folder, "dummies");
+        assert_eq!(cfg.output_folder, "output");
+        assert_eq!(cfg.quantity, 1);
+        assert_eq!(cfg.index_of_command, 0);
+    }
+    #[test]
+    fn config_no_file() {
+        let config = Configuration::new("NOFILE");
+        assert_err!(&config);
+    }
+
+    #[test]
+    fn config_missing_args_section() {
+        let config = Configuration::new("testfiles/dummyconfig/no_args_section.toml");
+        assert_err!(&config);
+    }
+    #[test]
+    fn config_syntax_error() {
+        let config = Configuration::new("testfiles/dummyconfig/bad_syntax.toml");
+        assert_err!(&config);
+    }
+    #[test]
+    fn config_missing_args_error() {
+        let config = Configuration::new("testfiles/dummyconfig/missing_args.toml");
+        assert_err!(&config);
     }
 }
